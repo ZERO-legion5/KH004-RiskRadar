688746b825e8ca282c62f3f7e03f19d0d40901e7
Sai Maharajan G <zerolegion5@gmail.com>
2024-02-04 01:52:10 +0530

    V8

--- server.py
+++ server.py
@@ -1,31 +1,68 @@
 import sqlite3
 import time
-from datetime import datetime
+import pandas as pd
+import random
 
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS data_table (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        value INTEGER,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-    )
+    CREATE TABLE IF NOT EXISTS KH(
+    trans_num VARCHAR(255),
+    merchant FLOAT,
+    category FLOAT,
+    amt FLOAT,
+    gender FLOAT,
+    lat FLOAT,
+    long FLOAT,
+    city_pop FLOAT,
+    job FLOAT,
+    age FLOAT,
+    timestamp_year FLOAT,
+    timestamp_month FLOAT,
+    timestamp_day FLOAT,
+    timestamp_hour FLOAT,
+    timestamp_min FLOAT,
+    timestamp_second FLOAT
+);
 ''')
 conn.commit()
 
-def insert_data(value):
-    cursor.execute('INSERT INTO data_table (value) VALUES (?)', (value,))
+def insert_data(dat):
+
+    s = 'INSERT INTO KH (trans_num, merchant, category, amt, gender, lat, long, city_pop, job, age, timestamp_year, timestamp_month, timestamp_day, timestamp_hour, timestamp_min, timestamp_second) VALUES ('
+    
+    # Properly format trans_num within the SQL query
+    s += f'"{dat["trans_num"]}", '
+
+    # Add the rest of the values
+    s += ', '.join(list(map(str, dat[1:])))
+    s += ');'
+    
+    cursor.execute(s)
     conn.commit()
+
+df = pd.read_csv('fraudDataAnon.csv', index_col=0)
+df.drop(columns= ['is_fraud'],inplace=True)
 
 try:
     while True:
-        data_value = 42
-        insert_data(data_value)
-        print(f"Data uploaded to database at {datetime.now()}")
+
+        dat = df.iloc[random.randint(0, df.shape[0] - 1), :]
+
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        print(dat)
+        insert_data(dat)
+
+        conn.commit()
+        conn.close()
+
         time.sleep(1)
 
 except KeyboardInterrupt:
     print("Exiting the program.")
+
 finally:
     conn.close()
